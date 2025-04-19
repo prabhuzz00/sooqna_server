077993b5cf9ba69a0cbbe3f6e082d31e74c3bf60
prabhuzz00 <prabhuzz00@gmail.com>
2025-04-19 19:38:35 +0530

    upload images

--- route/vendor.route.js
+++ route/vendor.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 import {
   createVendor,
   deleteVendor,
@@ -9,24 +10,13 @@ import {
   updateVendorStatus,
   vendorDetails,
   verifyVendor,
+  uploadImages,
+  uploadBannerImages,
 } from "../controllers/vendor.controller.js";
-import multer from "multer";
+import upload from "../middlewares/multer.js";
 import vendorAuth from "../middlewares/vendorAuth.js";
 
 const vendorRouter = express.Router();
-
-// Configure multer for file uploads
-const upload = multer({
-  dest: "uploads/",
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only images are allowed"));
-    }
-  },
-});
 
 // Route to create a vendor
 vendorRouter.post(
@@ -44,7 +34,19 @@ vendorRouter.patch(
 vendorRouter.delete("/:id", deleteVendor);
 vendorRouter.patch("/status/:id", updateVendorStatus);
 vendorRouter.post("/login", loginVendor);
-vendorRouter.get('/logout',vendorAuth,logoutVendor);
-vendorRouter.get('/vendor-details',vendorAuth,vendorDetails);
+vendorRouter.get("/logout", vendorAuth, logoutVendor);
+vendorRouter.get("/vendor-details", vendorAuth, vendorDetails);
+vendorRouter.post(
+  "/uploadImages",
+  vendorAuth,
+  upload.array("images"),
+  uploadImages
+);
+vendorRouter.post(
+  "/uploadBannerImages",
+  vendorAuth,
+  upload.array("bannerimages"),
+  uploadBannerImages
+);
 
 export default vendorRouter;
